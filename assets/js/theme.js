fda209c8c271f219ba1dab64f9ec6fe15148eba7
yxtang2020 <yxtang2020@gmail.com>
2024-07-28 01:19:25 +0800

    Update theme.js

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -1,48 +1,58 @@
-// Toggle between light and dark theme settings.
+// Has to be in the head tag, otherwise a flicker effect will occur.
+
+// Toggle through light, dark, and system theme settings.
 let toggleThemeSetting = () => {
   let themeSetting = determineThemeSetting();
-  if (themeSetting == "light") {
+  if (themeSetting == "system") {
+    setThemeSetting("light");
+  } else if (themeSetting == "light") {
     setThemeSetting("dark");
   } else {
-    setThemeSetting("light");
+    setThemeSetting("system");
   }
-  updateThemeIcon();
 };
 
 // Change the theme setting and apply the theme.
 let setThemeSetting = (themeSetting) => {
   localStorage.setItem("theme", themeSetting);
+
   document.documentElement.setAttribute("data-theme-setting", themeSetting);
+
   applyTheme();
 };
 
 // Apply the computed dark or light theme to the website.
 let applyTheme = () => {
-  let theme = determineThemeSetting();
+  let theme = determineComputedTheme();
 
   transTheme();
   setHighlight(theme);
   setGiscusTheme(theme);
   setSearchTheme(theme);
 
+  // if mermaid is not defined, do nothing
   if (typeof mermaid !== "undefined") {
     setMermaidTheme(theme);
   }
 
+  // if diff2html is not defined, do nothing
   if (typeof Diff2HtmlUI !== "undefined") {
     setDiff2htmlTheme(theme);
   }
 
+  // if echarts is not defined, do nothing
   if (typeof echarts !== "undefined") {
     setEchartsTheme(theme);
   }
 
+  // if vegaEmbed is not defined, do nothing
   if (typeof vegaEmbed !== "undefined") {
     setVegaLiteTheme(theme);
   }
 
   document.documentElement.setAttribute("data-theme", theme);
 
+  // Add class to tables.
   let tables = document.getElementsByTagName("table");
   for (let i = 0; i < tables.length; i++) {
     if (theme == "dark") {
@@ -52,6 +62,7 @@ let applyTheme = () => {
     }
   }
 
+  // Set jupyter notebooks themes.
   let jupyterNotebooks = document.getElementsByClassName("jupyter-notebook-iframe-container");
   for (let i = 0; i < jupyterNotebooks.length; i++) {
     let bodyElement = jupyterNotebooks[i].getElementsByTagName("iframe")[0].contentWindow.document.body;
@@ -64,9 +75,10 @@ let applyTheme = () => {
     }
   }
 
+  // Updates the background of medium-zoom overlay.
   if (typeof medium_zoom !== "undefined") {
     medium_zoom.update({
-      background: getComputedStyle(document.documentElement).getPropertyValue("--global-bg-color") + "ee",
+      background: getComputedStyle(document.documentElement).getPropertyValue("--global-bg-color") + "ee", // + 'ee' for trasparency.
     });
   }
 };
@@ -111,10 +123,14 @@ let addMermaidZoom = (records, observer) => {
 
 let setMermaidTheme = (theme) => {
   if (theme == "light") {
+    // light theme name in mermaid is 'default'
+    // https://mermaid.js.org/config/theming.html#available-themes
     theme = "default";
   }
 
+  /* Re-render the SVG, based on https://github.com/cotes2020/jekyll-theme-chirpy/blob/master/_includes/mermaid.html */
   document.querySelectorAll(".mermaid").forEach((elem) => {
+    // Get the code block content from previous element, since it is the mermaid code itself as defined in Markdown, but it is hidden
     let svgCode = elem.previousSibling.childNodes[0].innerHTML;
     elem.removeAttribute("data-processed");
     elem.innerHTML = svgCode;
@@ -133,6 +149,7 @@ let setMermaidTheme = (theme) => {
 
 let setDiff2htmlTheme = (theme) => {
   document.querySelectorAll(".diff2html").forEach((elem) => {
+    // Get the code block content from previous element, since it is the diff code itself as defined in Markdown, but it is hidden
     let textData = elem.previousSibling.childNodes[0].innerHTML;
     elem.innerHTML = "";
     const configuration = { colorScheme: theme, drawFileList: true, highlight: true, matching: "lines" };
@@ -143,6 +160,7 @@ let setDiff2htmlTheme = (theme) => {
 
 let setEchartsTheme = (theme) => {
   document.querySelectorAll(".echarts").forEach((elem) => {
+    // Get the code block content from previous element, since it is the echarts code itself as defined in Markdown, but it is hidden
     let jsonData = elem.previousSibling.childNodes[0].innerHTML;
     echarts.dispose(elem);
 
@@ -158,6 +176,7 @@ let setEchartsTheme = (theme) => {
 
 let setVegaLiteTheme = (theme) => {
   document.querySelectorAll(".vega-lite").forEach((elem) => {
+    // Get the code block content from previous element, since it is the vega lite code itself as defined in Markdown, but it is hidden
     let jsonData = elem.previousSibling.childNodes[0].innerHTML;
     elem.innerHTML = "";
     if (theme === "dark") {
@@ -186,36 +205,48 @@ let transTheme = () => {
   }, 500);
 };
 
+// Determine the expected state of the theme toggle, which can be "dark", "light", or
+// "system". Default is "system".
 let determineThemeSetting = () => {
   let themeSetting = localStorage.getItem("theme");
-  if (themeSetting != "dark" && themeSetting != "light") {
-    themeSetting = "light";
+  if (themeSetting != "dark" && themeSetting != "light" && themeSetting != "system") {
+    themeSetting = "system";
   }
   return themeSetting;
 };
 
-let updateThemeIcon = () => {
-  const mode_toggle = document.getElementById("light-toggle");
+// Determine the computed theme, which can be "dark" or "light". If the theme setting is
+// "system", the computed theme is determined based on the user's system preference.
+let determineComputedTheme = () => {
   let themeSetting = determineThemeSetting();
-  if (themeSetting == "dark") {
-    mode_toggle.textContent = "☀️"; // Sun icon for dark mode
+  if (themeSetting == "system") {
+    const userPref = window.matchMedia;
+    if (userPref && userPref("(prefers-color-scheme: dark)").matches) {
+      return "dark";
+    } else {
+      return "light";
+    }
   } else {
-    mode_toggle.textContent = "🌙"; // Moon icon for light mode
+    return themeSetting;
   }
 };
 
 let initTheme = () => {
   let themeSetting = determineThemeSetting();
-  setThemeSetting(themeSetting);
-  updateThemeIcon();
 
+  setThemeSetting(themeSetting);
+
+  // Add event listener to the theme toggle button.
   document.addEventListener("DOMContentLoaded", function () {
     const mode_toggle = document.getElementById("light-toggle");
 
     mode_toggle.addEventListener("click", function () {
       toggleThemeSetting();
     });
+  });
 
-    updateThemeIcon(); // Update icon on page load
+  // Add event listener to the system theme preference change.
+  window.matchMedia("(prefers-color-scheme: dark)").addEventListener("change", ({ matches }) => {
+    applyTheme();
   });
 };
